8c56dc9ae9384e4875aef80b2346d851aa3a0628
lolluslollus <lolluslollus@users.noreply.github.com>
2023-03-26 17:42:51 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -6,6 +6,7 @@ function data()
 [b]Priority signals for tracks.[/b]
 Place these before an intersection and ordinary signals (or a station) behind. Trains entering the intersection via priority signals will have priority over those entering via ordinary signals (or the station). Place the priority signal farther apart from the intersection to reserve a longer stretch of track for priority computations. Place the ordinary signals far enough from the intersection, so trains have room to stop.
 Tracks are made of subsequent segments, you can check them with debug mode and <AltGr + L>. If a signal is too many segments apart from an intersection, the priority computation will ignore it.
+Switch priority computations on and off with the icon in the bottom bar.
 			]],
             ["ModName"] = "Priority signals for tracks",
             ["PrioritySignalsOff"] = "Priority signals OFF",
