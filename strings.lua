bc3163eb437aa0c75674ab83a6db56cf99314d1f
lolluslollus <lolluslollus@users.noreply.github.com>
2023-03-28 22:43:12 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -5,14 +5,15 @@ function data()
                 [[
 [b]Priority signals for tracks.[/b]
 Place these before an intersection and ordinary signals (or a station) behind. Trains entering the intersection via priority signals will have priority over those entering via ordinary signals (or the station).
-If you want to prioritise a longer stretch of track, place a second priority signal farther ahead of the intersection (see notes).
+If you want to prioritise a longer stretch of track, place a second priority signal farther ahead of the intersection (but not too far, see the notes). If you have a station track immediately preceding a priority signal, the whole station track will receive priority as soon as the train starts.
 
-If you place no ordinary signals on one branch, trains entering the intersection from that branch will not give way. If you place an ordinary signal too close to an intersection, trains passing it will stop abruptly.
+If you place no ordinary signals on one branch, or you place them too far (see notes), trains entering the intersection from that branch will not give way. If you place an ordinary signal too close to an intersection, trains passing it will stop abruptly.
 
 [b]Notes:[/b]
-1) Tracks are made of subsequent segments, you can check them with debug mode and <AltGr + L>. If a signal is too many segments apart from an intersection, never mind how short they are, the priority computation will ignore it.
+1) Tracks are made of subsequent segments, you can check them with debug mode and <AltGr + L>. If a signal is too many segments apart from an intersection, never mind how short they are, the priority computation will ignore it. This is inconvenient but gives better performance.
 2) Every signal has a white arrow that appears when you open the signal menu. Two-way priority signals only prioritise trains running along the white arrow - not those running against it. This is akin to ordinary two-way signals and waypoints.
 3) Switch priority computations on and off with the icon in the bottom bar.
+4) Priority signals are expensive, only use them where you really need them.
 			]],
             ["ModName"] = "Priority signals for tracks",
             ["PrioritySignalsOff"] = "Priority signals OFF",
