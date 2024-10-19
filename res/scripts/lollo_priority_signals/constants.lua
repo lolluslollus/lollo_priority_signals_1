56900332598ce4c54fcdb65761e44e181650eeac
lolluslollus <lolluslollus@users.noreply.github.com>
2024-10-19 21:43:33 +0200

    Update constants.lua

--- res/scripts/lollo_priority_signals/constants.lua
+++ res/scripts/lollo_priority_signals/constants.lua
@@ -6,7 +6,8 @@ local constants = {
     maxNSegmentsBeforePrioritySignal = 2, -- seek segments max N segments ahead of priority signal
     maxNSegmentsBehindIntersection = 16, -- seek segments max N segments behind intersection
     maxDistanceFromIntersection = 500, -- seek edges max 500 m from intersection
-    maxDistanceForAdjacentNodes_by4 = 8000, -- (power 2 of the 2-fold of) the max distance between two nodes, to consider them adjacent -- LOLLO TODO this might need increasing
+    -- LOLLO TODO this might need increasing or removing the check altogether
+    maxDistanceForAdjacentNodes_by4 = 40000, -- (power 2 of the 2-fold of) the max distance between two nodes, to consider them adjacent
 
     numGetEdgeObjectsPerTick = 50,
     numGetGraphCoroutineResumesPerTick = 5,
